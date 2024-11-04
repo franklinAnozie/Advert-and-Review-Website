3f77b295c3fba4339be8e074e8c712f29c2c6c60
franklinAnozie <cfanozie@gmail.com>
2024-11-04 01:57:17 +0100

    created initial entry point of backend application

---
+++ backend/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import RedirectResponse
+from app.db.database import engine
+from app.models import models
+from app.routers import admin, adverts, reviews, users
+import os
+
+class TrailingSlashMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        if request.url.path.endswith('/') and request.url.path != '/':
+            url = str(request.url).rstrip('/')
+            return RedirectResponse(url, status_code=301)
+        response = await call_next(request)
+        return response
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.add_middleware(TrailingSlashMiddleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://advertorial-hub-frontend.vercel.app/",
+        "https://advertorial-hub-frontend.vercel.app",
+        "https://www.advertorial-hub-frontend.vercel.app/",
+        "https://www.advertorial-hub-frontend.vercel.app",
+        ],
+    allow_credentials=True,
+    allow_methods=[
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+        "OPTIONS"
+        ],
+    allow_headers=["*"],
+)
+
+UPLOAD_DIR = "/mnt/data/uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+app.mount(UPLOAD_DIR, StaticFiles(directory="/mnt/data/uploads"), name="uploads")
+
+app.include_router(admin.router)
+app.include_router(users.router)
+app.include_router(adverts.router, prefix="/api")
+app.include_router(reviews.router, prefix="/api")
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Advert and Review API"}
